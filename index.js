d1815529ccd2c6fb1ceeb8f3df46f356e61bed76
pratapP3453S <pratappanigrahy93@gmail.com>
2025-05-27 07:23:43 +0530

    feature break

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-const express = require('express');
-const cors = require('cors');
-const sequelize = require('./config/db');
-const authRoutes = require('./routes/authRoutes');
-const customerRoutes = require('./routes/customerRoutes');
-const bankerRoutes = require('./routes/bankerRoutes');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const sequelize = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const customerRoutes = require("./routes/customerRoutes");
+const bankerRoutes = require("./routes/bankerRoutes");
+require("dotenv").config();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://simplebankingapp.vercel.app/"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
-app.use('/api/customer', customerRoutes);
-app.use('/api/banker', bankerRoutes);
-app.use('/hello', bankerRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/customer", customerRoutes);
+app.use("/api/banker", bankerRoutes);
+app.use("/hello", bankerRoutes);
 
 sequelize.sync().then(() => {
-  app.listen(5000, () => console.log('Server running on http://localhost:5000'));
+  app.listen(5000, () =>
+    console.log("Server running on http://localhost:5000")
+  );
 });
